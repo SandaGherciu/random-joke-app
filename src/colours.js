8257befa4bfab6b9f00e21797ec1cf750c4b36b3
Sanda <sandagherciu@gmail.com>
2021-06-23 01:21:05 +0300

    first commit

---
+++ src/colours.js
@@ -0,0 +1,28 @@
+const colours = [
+  [
+    "#FFE5D9",
+    "#FFD7BA",
+    "#FEC89A",
+    "#FEC5BB",
+    "#FCD5CE",
+    "#FAE1DD",
+    "#F8EDEB",
+    "#E8E8E4",
+    "#D8E2DC",
+    "#ECE4DB",
+  ],
+  [
+    "#B39486",
+    "#B38C70",
+    "#B3845B",
+    "#B37C72",
+    "#B0867F",
+    "#AD8C87",
+    "#AB9591",
+    "#9C9C89",
+    "#81968A",
+    "#A19485",
+  ],
+];
+
+export default colours;
